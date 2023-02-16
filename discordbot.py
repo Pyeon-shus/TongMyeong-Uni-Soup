20575afbb9f9c9cba2707db0405b483876a98714
Pyeon-shus <125563050+Pyeon-shus@users.noreply.github.com>
2023-02-16 20:49:59 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -24,6 +24,35 @@ async def on_message(message):
 
     if message.content.startswith(f'{PREFIX}hello'):
         await message.channel.send('Hello!')
+    if message.content.startswith(f'{PREFIX}학식'): { ## "?"이라고 말했을때
+        import requests 
+        from bs4 import BeautifulSoup
+
+        #웹페이지를 요청합니다.
+        req = requests.get('https://www.tu.ac.kr/tuhome/diet.do?sch')
+
+        #HTML을 분석합니다.
+        soup = BeautifulSoup(req.text, 'html.parser')
+
+        #필요한 데이터를 가져옵니다.
+        table = soup.find('table', class_='table-st1')
+
+        #데이터를 리스트에 저장합니다.
+        data = []
+        for tr in table.find_all('tr'):
+            row = []
+            for td in tr.find_all('td'):
+               row.append(td.text)
+            data.append(row)
+
+        #봇에 출력하기 위해 리스트를 문자열로 변환합니다.
+        result = ''
+        for row in data:
+            result += '\n'.join(row) + '\n'
+
+        #결과를 봇으로 출력합니다.
+        msg.reply(result)
+
 
 
 try:

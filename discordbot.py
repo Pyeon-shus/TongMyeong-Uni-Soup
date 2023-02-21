a7f701cd00dab4a62d22f9421533be34e24f0feb
Pyeon-shus <125563050+Pyeon-shus@users.noreply.github.com>
2023-02-21 22:09:02 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -18,42 +18,46 @@ async def on_ready():
     print(f'Logged in as {client.user}.')
     channel = client.get_channel(969983391183282258) # 출력할 채널 ID를 넣어주세요
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+#@client.event
+#async def on_message(message):
+    #if message.author == client.user:
+    #    return
 
-    if message.content == 'call':
-        await channel.send("callback!")
+    #if message.content == 'call':
+    #    await channel.send("callback!")
 
-    if message.content.startswith('$hello'):
-        await channel.send('Hello!')
+    #if message.content.startswith('$hello'):
+    #    await channel.send('Hello!')
         
-    if message.content.startswith('$학식'):
+    #if message.content.startswith('$학식'):
         print(f'입력됨')
-        #웹페이지를 요청합니다.
-        url = 'https://www.tu.ac.kr/tuhome/diet.do?sch'
-        req = requests.get(url)
+    # 입력한 채팅을 삭제한다.
+    await message.delete()
+    
+    #웹페이지를 요청합니다.
+    url = 'https://www.tu.ac.kr/tuhome/diet.do?sch'
+    req = requests.get(url)
 
-        #HTML을 분석합니다.
-        soup = BeautifulSoup(req.text, 'html.parser')
+    #HTML을 분석합니다.
+    soup = BeautifulSoup(req.text, 'html.parser')
 
-        #필요한 데이터를 가져옵니다.
-        table = soup.find('table', class_='table-st1')
+    #필요한 데이터를 가져옵니다.
+    table = soup.find('table', class_='table-st1')
 
-        #데이터를 리스트에 저장합니다.
-        data = []
-        for tr in table.find_all('tr'):
-           row = []
-           for th in tr.find_all('th'):
-                 row.append(tr.text)
-           data.append(row)
+    #데이터를 리스트에 저장합니다.
+    data = []
+    for tr in table.find_all('tr'):
+       row = []
+       for th in tr.find_all('th'):
+             row.append(tr.text)
+       data.append(row)
 
-        #봇에 출력하기 위해 리스트를 문자열로 변환합니다.
-        result = ''
-        for row in data:
-            result += '\n'.join(row) + '\n'
-        await channel.send(result)
+    #봇에 출력하기 위해 리스트를 문자열로 변환합니다.
+    result = ''
+    for row in data:
+        result += '\n'.join(row) + '\n'
+    embed = discord.Embed(title="👋 :fork_and_knife:오늘의 식단:fork_and_knife:", description=result,timestamp=datetime.datetime.now(pytz.timezone('UTC')), color=0x4c2896)
+        #await channel.send(result)
         
 
 try:

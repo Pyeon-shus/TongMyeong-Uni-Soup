7e84b6ac3a549dcad6ef9f83755a8a2fbdadb423
Pyeon-shus <125563050+Pyeon-shus@users.noreply.github.com>
2023-02-22 14:18:32 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -44,8 +44,8 @@ async def on_message(message):
         #입력한 채팅을 삭제한다.
         #await message.delete()
         #user = message.mentions[1] # '!학식' 에서 유저정보를 user에 담는다.
-        print("{}".format(message.mentions[0]))
         #await channel.send("{} | {} 님이 '!학식'을 입력하셨습니다.".format(user.author, user.mention)) # 작성된 채널에 메세지를 출력한다.
+        await channel.send ("{} | {}님이 '!학식'을 입력하셨습니다.".format(channel.author, channel.author.mention)) # 작성된 채널에 메세지 출력
         
         #웹페이지를 요청합니다.
         url = 'https://www.tu.ac.kr/tuhome/diet.do?sch'

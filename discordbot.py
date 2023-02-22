ab05fe0d716d5de936dbb9b6bcac7994e099c448
Pyeon-shus <125563050+Pyeon-shus@users.noreply.github.com>
2023-02-22 12:24:56 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -13,6 +13,7 @@ PREFIX = os.environ['PREFIX']
 TOKEN = os.environ['TOKEN']
 channel_id = '969983391183282258'
 client = discord.Client(intents=discord.Intents.default())
+client = commands.Bot(command_prefix="!")
 
 @client.event
 async def on_ready():
@@ -25,44 +26,22 @@ async def on_ready():
     game = discord.Game('!학식 입력')
     await client.change_presence(status=discord.Status.online, activity=game)
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+class simple(commands.Cog): 
 
-    if message.content.startswith('!학식'):
-        print(f'입력됨')
-        #입력한 채팅을 삭제한다.
-        #await message.delete()
+    def __init__(self, app):
+        self.app = app 
+    
+    @commands.command(name="인사") # '!인사' 를 입력한다면
+    async def hi(self, ctx): 
+        await ctx.send("{} | {} 님 안녕하세요!".format(ctx.author, ctx.author.mention)) # 작성된 채널에 메세지를 출력한다.
+        await ctx.author.send("{} | {} 님 안녕하세요!".format(ctx.author, ctx.author.mention)) # 작성한 유저에게 DM으로 메세지를 출력한다.
 
-        #웹페이지를 요청합니다.
-        url = 'https://www.tu.ac.kr/tuhome/diet.do?sch'
-        req = requests.get(url)
-
-        #HTML을 분석합니다.
-        soup = BeautifulSoup(req.text, 'html.parser')
-
-        #필요한 데이터를 가져옵니다.
-        table = soup.find('table', class_='table-st1')
-
-        #데이터를 리스트에 저장합니다.
-        data = []
-        for tr in table.find_all('tr'):
-           row = []
-           for th in tr.find_all('th'):
-                 row.append(tr.text)
-           data.append(row)
-
-        #봇에 출력하기 위해 리스트를 문자열로 변환합니다.
-        result = ''
-        for row in data:
-            result += '\n'.join(row) + '\n'
+    @commands.command(name="식단") # '!식단'를 입력한다면
+    async def information(self,ctx):
         embed = discord.Embed(title=":fork_and_knife:오늘의 식단:fork_and_knife:", description=result,timestamp=datetime.datetime.now(pytz.timezone('UTC')), color=0x4c2896)
         embed.set_thumbnail(url="https://cdn-icons-png.flaticon.com/512/4474/4474873.png")
         embed.set_footer(text="Bot Made by. Shus#7777, , 문의는 DM으로 부탁드립니다 💬")
-        await messae.channel.send (embed=embed) #채팅방에 출력되도록 하려면 messae.channel.send 로 바꾸면 된다.
-        #await message.author.send (embed=embed) #유저 개인 DM으로 전송한다.
-        #await channel.send(result)
+        await ctx.send(embed=embed)
 
         
 try:

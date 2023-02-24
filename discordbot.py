b7957f62c83f55116a5c4ab4050615aaa689fda6
Pyeon-shus <125563050+Pyeon-shus@users.noreply.github.com>
2023-02-24 14:21:13 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -122,7 +122,7 @@ async def on_message(message):
         for row in ilpum:
             result += '\n'.join(row) + '\n'
         embed.add_field(name="#일품", value=f"{result}", inline=False)
-        embed.set_footer(text="Bot Made by. Shus#7777, 식단 출처: {}".format(url))
+        embed.set_footer(text="Bot Made by. Shus#7777, 식단 출처: {url}")
         await channel.send (embed=embed) #채팅방에 출력되도록 하려면 messae.channel.send 로 바꾸면 된다.
         #await message.author.send (embed=embed) #유저 개인 DM으로 전송한다.
         #await channel.send(result)

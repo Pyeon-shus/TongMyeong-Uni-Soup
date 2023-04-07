c20d7da23daca3d36047f7862597a16fb8a73c44
Pyeon-shus <125563050+Pyeon-shus@users.noreply.github.com>
2023-04-08 02:42:32 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -387,16 +387,18 @@ async def on_message(message):
                     if date_value == today:
                         # 학식 메뉴 추출
                         menu_list = box.find_all("ul", {"class": "b-cal-undergrad"})
+                        
+                    embed = discord.Embed(title=":fork_and_knife:오늘의 숙식:fork_and_knife:", description="",timestamp=datetime.datetime.now(pytz.timezone('UTC')), color=0x00b992)
+                    embed.set_thumbnail(url="https://cdn-icons-png.flaticon.com/512/4916/4916579.png")
+                    embed.add_field(name="\n", value=f"\n", inline=False)
+                    embed.add_field(name="#조식", value=f"\n", inline=False)
 
                         # 메뉴 출력
                         for menu in menu_list:
                             # 메뉴 항목 추출
                             menu_items = menu.find_all("li")
                             # 메뉴 출력
-                            embed = discord.Embed(title=":fork_and_knife:오늘의 숙식:fork_and_knife:", description="",timestamp=datetime.datetime.now(pytz.timezone('UTC')), color=0x00b992)
-                            embed.set_thumbnail(url="https://cdn-icons-png.flaticon.com/512/4916/4916579.png")
-                            embed.add_field(name="\n", value=f"\n", inline=False)
-                            embed.add_field(name="#조식", value=f"\n", inline=False)
+                            
                             for item in menu_items:
                                 menu_text = item.text.strip()
                                 time_meal, menu_ = menu_text.split(":")
@@ -404,14 +406,14 @@ async def on_message(message):
                                     embed.add_field(name="", value=f"{menu_}\n\n", inline=False)
                                 elif time_meal in ["석식"]:
                                     embed.add_field(name="#석식", value=f"{menu_}\n\n", inline=False)
-                            embed.add_field(name=" ", value=f"⚠️!숙식은 현재 불안정 합니다 차후 수정할 계획입니다.⚠️\n", inline=False)
-                            embed.add_field(name="\n", value=f"\n", inline=False)
-                            embed.add_field(name=" ", value=f"식단 출처: {url}\n\n", inline=False)
-                            embed.set_footer(text="Bot Made by. Shus#7777, 자유롭게 이용해 주시면 됩니다.")
-                            await channel.send (embed=embed) #채팅방에 출력되도록 하려면 messae.channel.send 로 바꾸면 된다.
-                            #await message.author.send (embed=embed) #유저 개인 DM으로 전송한다.
-                            #await channel.send(result)
-                            print(f'정상 출력됨\n')
+                    embed.add_field(name=" ", value=f"⚠️!숙식은 현재 불안정 합니다 차후 수정할 계획입니다.⚠️\n", inline=False)
+                    embed.add_field(name="\n", value=f"\n", inline=False)
+                    embed.add_field(name=" ", value=f"식단 출처: {url}\n\n", inline=False)
+                    embed.set_footer(text="Bot Made by. Shus#7777, 자유롭게 이용해 주시면 됩니다.")
+                    await channel.send (embed=embed) #채팅방에 출력되도록 하려면 messae.channel.send 로 바꾸면 된다.
+                    #await message.author.send (embed=embed) #유저 개인 DM으로 전송한다.
+                    #await channel.send(result)
+                    print(f'정상 출력됨\n')
 
 #------------------------------------------------------------------------------------------------------------------------------------------------
 

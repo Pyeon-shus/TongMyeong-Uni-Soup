d153e2e7dfb0876391a82111183847d443a696e6
Pyeon-shus <125563050+Pyeon-shus@users.noreply.github.com>
2023-04-09 13:20:19 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -415,26 +415,26 @@ async def on_message(message):
                                     dinner_menu_str += "\n"
                                 dinner_menu_str += menu_split[1]
                                     
-                    if len(breakfast_menu_str) > 0:
-                        embed.add_field(name="#조식", value=f"{breakfast_menu_str}\n\n", inline=False)
-                    else:
-                        embed.add_field(name="#조식", value=f"오늘 조식 메뉴가 없습니다.\n\n", inline=False)
-                    embed.add_field(name="\n", value=f"\n", inline=False)
-                    
-                    if len(dinner_menu_str) > 0:
-                        embed.add_field(name="#석식", value=f"{dinner_menu_str}\n\n", inline=False)
-                    else:
-                        embed.add_field(name="#석식", value=f"오늘 석식 메뉴가 없습니다.\n\n", inline=False)
-                    embed.add_field(name="\n", value=f"\n", inline=False)
-                    
-                    #embed.add_field(name=" ", value=f"⚠️!숙식은 현재 불안정 합니다 차후 수정할 계획입니다.⚠️\n", inline=False)
-                    embed.add_field(name="\n", value=f"\n", inline=False)
-                    embed.add_field(name=" ", value=f"식단 출처: {url}\n\n", inline=False)
-                    embed.set_footer(text="Bot Made by. Shus#7777, 자유롭게 이용해 주시면 됩니다.")
-                    await channel.send (embed=embed) #채팅방에 출력되도록 하려면 messae.channel.send 로 바꾸면 된다.
-                    #await message.author.send (embed=embed) #유저 개인 DM으로 전송한다.
-                    #await channel.send(result)
-                    print(f'정상 출력됨\n')
+                if len(breakfast_menu_str) > 0:
+                    embed.add_field(name="#조식", value=f"{breakfast_menu_str}\n\n", inline=False)
+                else:
+                    embed.add_field(name="#조식", value=f"오늘 조식 메뉴가 없습니다.\n\n", inline=False)
+                embed.add_field(name="\n", value=f"\n", inline=False)
+                
+                if len(dinner_menu_str) > 0:
+                    embed.add_field(name="#석식", value=f"{dinner_menu_str}\n\n", inline=False)
+                else:
+                    embed.add_field(name="#석식", value=f"오늘 석식 메뉴가 없습니다.\n\n", inline=False)
+                embed.add_field(name="\n", value=f"\n", inline=False)
+                
+                #embed.add_field(name=" ", value=f"⚠️!숙식은 현재 불안정 합니다 차후 수정할 계획입니다.⚠️\n", inline=False)
+                embed.add_field(name="\n", value=f"\n", inline=False)
+                embed.add_field(name=" ", value=f"식단 출처: {url}\n\n", inline=False)
+                embed.set_footer(text="Bot Made by. Shus#7777, 자유롭게 이용해 주시면 됩니다.")
+                await channel.send (embed=embed) #채팅방에 출력되도록 하려면 messae.channel.send 로 바꾸면 된다.
+                #await message.author.send (embed=embed) #유저 개인 DM으로 전송한다.
+                #await channel.send(result)
+                print(f'정상 출력됨\n')
 
 #------------------------------------------------------------------------------------------------------------------------------------------------
 
